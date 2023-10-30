a58178ddf1df3b02c6e1c4cbc2d68e9ac606cfdb
haufung80 <47497069+haufung80@users.noreply.github.com>
2023-10-30 00:33:06 -0400

    bybit order execute

--- src/components/actions/bybit_order_execute.py
+++ src/components/actions/bybit_order_execute.py
@@ -1,13 +1,29 @@
 from components.actions.base.action import Action
+import ccxt as ccxt
+import configparser
 
 
 class BybitOrderExecute(Action):
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    API_KEY = config['BybitSettings']['key']
+    API_SECRET = config['BybitSettings']['secret']
+    exchange = ccxt.bybit({
+        'apiKey': API_KEY,
+        'secret': API_SECRET
+    })
     def __init__(self):
         super().__init__()
+        self.exchange.check_required_credentials()  # raises AuthenticationError
+
+    def place_order(self, data):
+        markets = self.exchange.load_markets()
+        formatted_amount = self.exchange.amount_to_precision(data['symbol'], data['amount'])
+        order = self.exchange.create_market_order(data['symbol'], data['action'], formatted_amount)
+        print(order)
 
     def run(self, *args, **kwargs):
         super().run(*args, **kwargs)  # this is required
-        """
-        Custom run method. Add your custom logic here.
-        """
         print(self.name, '---> action has run!')
+        data = self.validate_data()
+        self.place_order(data['data'])
