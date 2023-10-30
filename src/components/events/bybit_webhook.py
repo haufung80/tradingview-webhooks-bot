2ebe25579acf397dcdf26fee6324b26843e8d835
haufung80 <47497069+haufung80@users.noreply.github.com>
2023-10-29 20:56:25 -0400

    create order execution for bybit

---
+++ src/components/events/bybit_webhook.py
@@ -0,0 +1,6 @@
+from components.events.base.event import Event
+
+
+class BybitWebhook(Event):
+    def __init__(self):
+        super().__init__()
--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,9 @@
 # actions
-REGISTERED_ACTIONS = ['PrintData', 'IBOrderExecute']
+REGISTERED_ACTIONS = ['PrintData', 'IBOrderExecute', 'BybitOrderExecute']
 
 # events
-REGISTERED_EVENTS = ['IBWebhook', 'WebhookReceived']
+REGISTERED_EVENTS = ['BybitWebhook', 'WebhookReceived', 'IBWebhook']
 
 # links
-REGISTERED_LINKS = [('IBOrderExecute', 'IBWebhook'), ('PrintData', 'WebhookReceived')]
+REGISTERED_LINKS = [('IBOrderExecute', 'IBWebhook'), ('BybitOrderExecute', 'BybitWebhook'), ('PrintData', 'WebhookReceived')]
 
