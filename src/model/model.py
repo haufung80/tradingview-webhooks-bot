bafb6921795e9f39cfb40c5734a2cb4fdffd4e24
Hayden Tang <haufung80@gmail.com>
2024-01-09 02:17:40 -0500

    install orm and create strategy table

---
+++ src/model/model.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column, String, Integer, Float
+from sqlalchemy import Column, DateTime, func
+from sqlalchemy.dialects.mysql import INTEGER
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class BaseMixin:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            if key in dir(self):
+                exec(f"self.{key} = {value}")
+
+    id = Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now())
+
+
+class Strategy(Base, BaseMixin):
+    __tablename__ = 'strategy'
+
+    strategy_id = Column(String(50))
+    position_size = Column(Float())
+    parameter_1 = Column(String(50))
+    value_1 = Column(String(50))
+    parameter_2 = Column(String(50))
+    value_2 = Column(String(50))
+    parameter_3 = Column(String(50))
+    value_3 = Column(String(50))
+    parameter_4 = Column(String(50))
+    value_4 = Column(String(50))
