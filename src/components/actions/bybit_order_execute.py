32a9931f071189ec28b4fbdd73e856428b5f27b2
Hayden Tang <haufung80@gmail.com>
2024-01-09 19:50:08 -0500

    add set_sandbox_mode flag insert to Alert History table

--- src/components/actions/bybit_order_execute.py
+++ src/components/actions/bybit_order_execute.py
@@ -1,6 +1,19 @@
 from components.actions.base.action import Action
 import ccxt as ccxt
 import configparser
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+import os
+import sys
+from dotenv import load_dotenv
+from datetime import datetime
+
+sys.path.append(os.path.split(os.getcwd())[0])
+
+load_dotenv()
+engine = create_engine(os.getenv('POSTGRESQL_URL'), echo=True)
+
+from src.model.model import AlertHistory
 
 
 class BybitOrderExecute(Action):
@@ -12,6 +25,8 @@ class BybitOrderExecute(Action):
         'apiKey': API_KEY,
         'secret': API_SECRET
     })
+
+    exchange.set_sandbox_mode(config['BybitSettings']['set_sandbox_mode'])
     def __init__(self):
         super().__init__()
         self.exchange.check_required_credentials()  # raises AuthenticationError
@@ -19,14 +34,26 @@ class BybitOrderExecute(Action):
 
     def place_order(self, data):
         symbol = None
-
         if data['symbol'] == 'BTCUSDT.P':
             symbol = 'BTCUSDT'
         elif data['symbol'] == 'BNBUSDT.P':
             symbol = 'BNBUSDT'
 
+        with Session(engine) as session:
+            session.add(AlertHistory(
+                source=data['source'],
+                message_payload=data,
+                strategy_id=data['strategy_id'],
+                timestamp=datetime.fromtimestamp(data['timestamp']),
+                symbol=data['symbol'],
+                exchange=data['exchange'],
+                action=data['action'],
+                price=data['price'],
+            ))
+            session.commit()
+
         formatted_amount = self.exchange.amount_to_precision(symbol, float(data['amount']))
-        order = self.exchange.create_market_order(symbol, data['action'], formatted_amount)
+        order = self.exchange.create_limit_order(symbol, data['action'], formatted_amount, data['price'])
         print(order)
 
     def run(self, *args, **kwargs):
