0b5440cd666ec09c46f3c04279190a16aa837f1c
Hayden Tang <haufung80@gmail.com>
2024-03-25 16:37:47 -0400

    debug

--- src/model/model.py
+++ src/model/model.py
@@ -248,6 +248,7 @@ class BybitFetchOrderResponse(FetchOrderResponse):
         self.payload = str(resp)
 
         self.updated_time = int(resp['info']['updatedTime'])
+        self.avg_price = 0  # assume avg_price is 0
         if self.order_status == ExchangeOrderStatus.BYBIT_FILLED.value or self.order_status == ExchangeOrderStatus.BYBIT_PARTIALLY_FILLED.value:
             self.avg_price = float(resp['info']['avgPrice'])
         self.cum_exec_value = float(resp['info']['cumExecValue'])
