04e60e2d826352e8144b0e7499ac16048e37e2cd
Hayden Tang <haufung80@gmail.com>
2024-03-25 16:43:12 -0400

    min amount for FET in OKEX

--- src/components/actions/bybit_order_execute.py
+++ src/components/actions/bybit_order_execute.py
@@ -429,6 +429,8 @@ class BybitOrderExecute(Action):
                     exchange_symbol == 'LUNC/USDT':
                 okex_odr_price = alrt.price / 1000
                 formatted_amount = exchange.amount_to_precision(exchange_symbol, 1000 * amount)
+            elif exchange_symbol == 'FET/USDT' and amount < 10:
+                formatted_amount = 10
             else:
                 formatted_amount = exchange.amount_to_precision(exchange_symbol, amount)
             try:
