577d2b10f2f9a6f7bc8ba275961868a2fc533dbd
Hayden Tang <haufung80@gmail.com>
2024-03-27 14:08:38 -0400

    floki min amount okex 2, account for fee

--- src/components/actions/bybit_order_execute.py
+++ src/components/actions/bybit_order_execute.py
@@ -429,7 +429,7 @@ class BybitOrderExecute(Action):
                     exchange_symbol == 'LUNC/USDT':
                 okex_odr_price = alrt.price / 1000
                 if exchange_symbol == 'FLOKI/USDT' and (1000 * amount) < 100000:
-                    formatted_amount = 100000
+                    formatted_amount = 110000
                 else:
                     formatted_amount = exchange.amount_to_precision(exchange_symbol, 1000 * amount)
             elif exchange_symbol == 'FET/USDT' and amount < 10:
