71454b4da2ad46bee32e6f5fc2aa6120edc22ee8
haufung <haufung80@gmail.com>
2024-06-01 03:13:34 -0400

    bitget bug fix

--- src/components/actions/bybit_order_execute.py
+++ src/components/actions/bybit_order_execute.py
@@ -383,17 +383,17 @@ class BybitOrderExecute(Action):
 
         elif strategy_mgmt.exchange == CryptoExchange.BITGET.value:
             bitget_odr_price = alrt.price
-            if exchange_symbol == 'SBTC/SUSDT:SUSDT' and amount < 0.005:
-                formatted_amount = 0.001
-            elif exchange_symbol == 'SETH/SUSDT:SUSDT' and amount < 0.05:
+            if exchange_symbol == 'SBTC/SUSDT:SUSDT' and amount < 0.002:
+                formatted_amount = 0.002
+            elif exchange_symbol == 'SETH/SUSDT:SUSDT' and amount < 0.01:
                 formatted_amount = 0.01
-            elif exchange_symbol == 'BTC/USDT:USDT' and amount < 0.005:
-                formatted_amount = 0.001
-            elif exchange_symbol == 'WBTC/USDT' and amount < 0.005:
-                formatted_amount = 0.001
-            elif exchange_symbol == 'ETH/USDT:USDT' and amount < 0.05:
+            elif exchange_symbol == 'BTC/USDT:USDT' and amount < 0.002:
+                formatted_amount = 0.002
+            elif exchange_symbol == 'WBTC/USDT' and amount < 0.002:
+                formatted_amount = 0.002
+            elif exchange_symbol == 'ETH/USDT:USDT' and amount < 0.01:
                 formatted_amount = 0.01
-            elif exchange_symbol == 'STETH/USDT:USDT' and amount < 0.05:
+            elif exchange_symbol == 'STETH/USDT:USDT' and amount < 0.01:
                 formatted_amount = 0.01
             elif exchange_symbol == 'SOL/USDT:USDT' and amount < 1:
                 formatted_amount = 0.1
